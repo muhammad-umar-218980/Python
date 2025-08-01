919bf1b7ddc20310d1c35e55c89e8fb092f6df52
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-08-01 19:18:08 +0500

    added output lines

--- Fundamentals/Data Types/Strings/String Methods/string_methods_casing.py
+++ Fundamentals/Data Types/Strings/String Methods/string_methods_casing.py
@@ -1,11 +1,6 @@
 # Casing methods: capitalize, lower, upper, title, swapcase, casefold, istitle, isupper, islower.
 
-examples = [
-    "hello world", 
-    "PYTHON Programming", 
-    "ßharp", 
-    "This Is Title"
-]
+examples = ["hello world", "PYTHON Programming", "ßharp", "This Is Title"]
 
 for text in examples:
     print(f"Original: {text}")
@@ -19,3 +14,53 @@ for text in examples:
     print(" isupper(): ", text.isupper())
     print(" istitle(): ", text.istitle())
     print("-" * 40)
+
+
+"""# Output:
+
+Original: hello world
+ capitalize(): Hello world
+ lower():    hello world
+ upper():    HELLO WORLD
+ swapcase(): HELLO WORLD
+ title():    Hello World
+ casefold(): hello world
+ islower():  True
+ isupper():  False
+ istitle():  False
+----------------------------------------
+Original: PYTHON Programming
+ capitalize(): Python programming
+ lower():    python programming
+ upper():    PYTHON PROGRAMMING
+ swapcase(): python pROGRAMMING
+ title():    Python Programming
+ casefold(): python programming
+ islower():  False
+ isupper():  False
+ istitle():  False
+----------------------------------------
+Original: �harp
+ capitalize(): Ssharp
+ lower():    �harp
+ upper():    SSHARP
+ swapcase(): SSHARP
+ title():    Ssharp
+ casefold(): ssharp
+ islower():  True
+ isupper():  False
+ istitle():  False
+----------------------------------------
+Original: This Is Title
+ capitalize(): This is title
+ lower():    this is title
+ upper():    THIS IS TITLE
+ swapcase(): tHIS iS tITLE
+ title():    This Is Title
+ casefold(): this is title
+ islower():  False
+ isupper():  False
+ istitle():  True
+----------------------------------------
+
+"""
